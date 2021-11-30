cceb245338c4bc9cf8f471ce58df6a97cb570840
Paulo Salvatore <salvatoregames@gmail.com>
2021-11-29 21:24:31 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -25,6 +25,8 @@ namespace dotnetcore
 
             // Tipos de informações
             // Texto - String - Fica dentro de aspas duplas
+            // Número - inteiro - Int, Long - Principal: Int
+            // Número - decimal - Float, Double, Decimal - Principal: Double
 
 
             // Exemplo 1
@@ -64,11 +66,66 @@ namespace dotnetcore
 
             // Exemplo 4
 
+            /*
             Console.WriteLine("Digite o seu nome:");
 
             string nome = Console.ReadLine();
 
             Console.WriteLine($"Olá, {nome}!");
+            */
+
+            // Exemplo 5
+            
+            // Número - inteiro - Int, Long - Principal: Int
+            // Número - decimal - Float, Double, Decimal - Principal: Double
+
+            /*
+            var nome = "Paulo";
+            var idade = 10;
+            var altura = 1.8;
+
+            Console.WriteLine($"Olá, {nome}, você possui {idade} anos de idade e mede {altura}cm.");
+            */
+
+            // Operações matemáticas
+            /*
+            var soma = 10 + 5;
+            var subtracao = 10 - 5;
+            var multiplicacao = 10 * 2;
+            var divisao = 10.0 / 3;
+
+            Console.WriteLine($"Soma: {soma}");
+            Console.WriteLine($"Subtração: {subtracao}");
+            Console.WriteLine($"Multiplicação: {multiplicacao}");
+            Console.WriteLine($"Divisão: {divisao}");
+            */
+
+            // Desafio 1
+            // Calculadora dos 10% do Garçom
+            // Declarar uma variável com o valor da conta
+            // Declarar uma variável com o valor da taxa adicional do Garçom
+            // Declarar uma variável com o resultado dessa operação, adicionando a porcentagem da taxa no valor da conta
+            // Essa terceira variável deve utilizar o valor das duas primeiras para fazer o cálculo
+            // Ao final, exiba quanto ficou o valor total da conta
+
+            /*
+            10              por cento
+            10              /   100
+            porcentagemTaxa /   100
+            */
+
+            var valorConta = 45.35;
+            var porcentagemTaxa = 10;
+
+            var valorTaxa = valorConta * porcentagemTaxa / 100;
+
+            var valorTotal = valorConta + valorTaxa;
+
+            // Resultado de 45.35 + 10% deve dar: 49.885
+
+            Console.WriteLine($"Valor da conta foi de R{valorConta:C}."); // Currency
+            Console.WriteLine($"A taxa do garçom foi de R{valorTaxa:C}.");
+            Console.WriteLine($"A conta ficou no total de R{valorTotal:C}.");
         }
     }
 }
